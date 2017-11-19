5dddefbf65d7a48f56c521c1e004522320ccecda
Ardalan Amini <ardalan1074@gmail.com>
2017-11-19 18:44:46 +0330

    nothing

--- dist/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var PropTypes = require("prop-types");
-var elementOf = function (element) { return PropTypes.shape({ type: PropTypes.oneOf([element]) }); };
-exports.default = { elementOf: elementOf };
